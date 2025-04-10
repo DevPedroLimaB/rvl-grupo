670c99fd422423367271c1610025f54a3f4a2373
Heitor Filizzola <76500311+heitorfilizzola@users.noreply.github.com>
2025-04-10 15:01:49 -0300

    feat: services page

---
+++ services_page/script.js
@@ -0,0 +1,27 @@
+var coll = document.getElementsByClassName("collapsible");
+
+    //Tradução 
+    function setLanguage(lang) {
+        document.querySelectorAll('[data-pt]').forEach(el => {
+          el.innerText = el.getAttribute(`data-${lang}`);
+        });
+      }
+    
+      window.setLanguage = setLanguage;
+    
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        
+
+        const parentElement = this.closest('.toggle_element');
+        const content = parentElement.querySelector('.content');
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
